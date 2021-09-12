651c18018ce3333e871389126c3475e2b536a012
Mayank Saxena <83663500+Mayank-Dev-23@users.noreply.github.com>
2021-09-12 08:53:00 +0530

    Add files via upload

---
+++ Skills.js
@@ -0,0 +1,36 @@
+import Bar from "./Bar"
+
+const skill=[
+    {title:"Reactjs", width:"384px"},
+    {title:"Html5", width:"350px"},
+    {title:"Git" , width:"240px"},
+    {title:"Redux" ,width:"280px"},
+    {title:"Java script", width:"340px"},
+    {title:"Css", width:"320px"},
+    {title:"Figma", width:"250px"},
+]
+
+function Skills() {
+    return (
+        <div className="h-auto w-full pt-10  p-10 md:p-0 " id="skills">
+<div className="flex flex-col  items-center pt-10 px-4 ">
+            <h1 className="text-2xl uppercase">Skills</h1>
+            <div className="h-1 w-32 bg-black  "></div>
+           <p className="pt-8 skillhead font1">My Programming Language Proficiency</p>
+            </div>
+
+<div className="flex   items-center flex-col   sm:shadow-xl  mt-10 border-2 border-white  border-opacity-30 rounded-sm">
+    {skill.map((item,index)=>(
+        <Bar  key={index} item={item}/>
+    ))}
+</div>
+          
+
+         
+            
+            
+        </div>
+    )
+}
+
+export default Skills
